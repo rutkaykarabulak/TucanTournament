ea2a2420c9340f4208121c6086f9739755065336
rutkaykarabulak <rutkay.karabulak@inbestme.com>
2023-01-19 22:42:51 +0100

    Adding utils

---
+++ TucanTournament/Utils/Types.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TucanTournament.Utils
+{
+	public class Types
+	{
+		/// <summary>
+		/// Type of the sport.
+		/// You can extend this enum to add more sport.
+		/// </summary>
+		public enum SportType
+		{
+			Basketball, // 0
+			Handball // 1
+		}
+	}
+}
